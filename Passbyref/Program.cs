9b0dbcf2814206fcd3462f65b1f5fbbde229603d
Ramyamani9975 <ramyamanivannan99@gmail.com>
2023-02-07 13:22:53 +0530

    Add project files.

---
+++ Passbyref/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Passbyref
+{
+    internal class Program
+    {
+        void Add(ref int var)
+        {
+            var = var + 10;
+
+        }
+        void add1(int var1)
+        {
+            var1++;
+            Console.WriteLine(var1);
+            
+
+        }
+        static void Main(string[] args)
+        {
+        
+            Program p = new Program ();
+            int num = 12;
+            p.Add(ref num);
+            Console.WriteLine(num);
+            p.add1(num);
+            Console.WriteLine(num);
+            Console.ReadLine();
+        }
+       
+    }
+}
